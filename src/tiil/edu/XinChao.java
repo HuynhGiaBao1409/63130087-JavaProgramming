7ec3d071204105f80f3e482a3b1649cee80aa97f
HuynhGiaBao1409 <bao.hg.63cntt@ntu.edu.vn>
2024-01-19 15:44:34 +0700

    test

---
+++ src/tiil/edu/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//
+		System.out.println("Hello World");
+
+	}
+
+}
